e6119be5d2f5e3172b088cf95b508b72af08727e
Ren <crycatdo@gmail.com>
2020-03-09 20:19:18 +0200

    master

--- views/Home.js
+++ views/Home.js
@@ -1,21 +1,17 @@
 import React from 'react';
 import List from '../components/List';
 import PropTypes from 'prop-types';
-import { View, Text } from 'react-native'
 
 const Home = (props) => {
-    // console.log('Home', props);
-    const { navigation } = props;
-    return (
-        // <List navigation={navigation} mode={'all'}></List>
-      <View><Text>
-        Hello
-      </Text></View>
-    );
+  // console.log('Home', props);
+  const {navigation} = props;
+  return (
+    <List navigation={navigation} mode={'all'}></List>
+  );
 };
 
 Home.propTypes = {
-    navigation: PropTypes.object,
+  navigation: PropTypes.object,
 };
 
 export default Home;
